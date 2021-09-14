5cd403bdd389f80cc0d52fac5a31037d1b686cb0
Nathan Sadler <nathan@sadlerville.com>
2021-09-14 14:34:46 -0400

    bring over and adapt card tests

---
+++ src/__tests__/Card.test.js
@@ -0,0 +1,111 @@
+import Card from '../Card.js'
+import Game from './../Game.js'
+import Player from './../Player.js'
+import Deck from './../Deck.js'
+import TurnResult from './../TurnResult.js'
+import BotPlayer from './../BotPlayer.js'
+
+describe('Card', () => {
+  let testCard
+
+  beforeEach(() => {
+    testCard = new Card("10", "H")
+  })
+
+  describe('#constructor', () => {
+    it("sets the card's rank", () => {
+      var card = new Card("4", "S")
+      expect(card._rank).toEqual("4")
+    })
+
+    it("sets the card's suit", () => {
+      var card = new Card("4", "D")
+      expect(card._suit).toEqual("D")
+    })
+  })
+
+  describe('#describe', () => {
+    it("returns a string in the form 'rank' of 'suit's", () => {
+      var card = new Card("4", "D")
+      expect(card.describe()).toEqual('4 of Diamonds')
+    })
+
+    it("uses the name of the rank if the rank it has is just a letter", () => {
+      var card = new Card("Q", "H")
+      expect(card.describe()).toEqual("Queen of Hearts")
+    })
+  })
+
+  describe('#describeRank', () => {
+    let card
+
+    it('returns the rank of the card unmodified if it is from 2 to 10', () => {
+      // for(let card_rank = 2; card_rank < 11; card_rank++) {
+      //   card = new Card(card_rank.toString(), "D")
+      //   expect(card.describeRank()).toEqual(card_rank.toString())
+      // }
+      [...Array(9)].forEach((_, card_rank) => {
+        card = new Card(card_rank.toString(), "D")
+        expect(card.describeRank()).toEqual(card_rank.toString())
+      })
+    })
+
+    it('returns the name of the rank if it is not a number', () => {
+      card = new Card('Q', 'S')
+      expect(card.describeRank()).toEqual('Queen')
+    })
+  })
+
+  describe('#describeSuit', () => {
+    let card
+    
+    it("returns the name of the card's suit", () => {
+      const possible_suits = ['C', 'D', 'H', 'S']
+      const possible_suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+      new Array(0, 1, 2, 3).forEach((_, index) => {
+        card = new Card("4", possible_suits[index])
+        expect(card.describeSuit()).toEqual(possible_suit_names[index])
+      })
+    })
+  })
+
+  describe('#equals', () => {
+    let comparisonCard
+
+    beforeEach(() => {
+      comparisonCard = new Card('10', 'H')
+    })
+
+    it('is true if the ranks and suits of both cards match', () => {
+      expect(testCard.equals(comparisonCard)).toEqual(true)
+    })
+
+    it("is false if the cards' ranks aren't the same", () => {
+      comparisonCard._rank = '9'
+      expect(testCard.equals(comparisonCard)).toEqual(false)
+    })
+
+    it("is false if the cards' suits aren't the same", () => {
+      comparisonCard._suit = 'D'
+      expect(testCard.equals(comparisonCard)).toEqual(false)
+    })
+  })
+
+  describe('#generateId', () => {
+    it("returns a string in the form of 'rank_suit'", () => {
+      expect(testCard.generateId()).toEqual('10_H')
+    })
+  })
+
+  describe('#rank', () => {
+    it('returns the rank of the card', () => {
+      expect(testCard.rank()).toEqual("10")
+    })
+  })
+
+  describe('#suit', () => {
+    it('returns the suit of the card', () => {
+      expect(testCard.suit()).toEqual("H")
+    })
+  })
+})
