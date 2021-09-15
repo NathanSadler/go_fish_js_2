e1d2789130fed737c7d5b9a5f9a2a0feb5f57567
Nathan Sadler <nathan@sadlerville.com>
2021-09-15 11:29:49 -0400

    re-add BotPlayer

---
+++ src/__tests__/BotPlayer.test.js
@@ -0,0 +1,47 @@
+import Card from '../Card.js'
+import Player from '../Player.js'
+import BotPlayer from './../BotPlayer.js'
+import Game from '../Game.js'
+
+describe('BotPlayer', () => {
+  let testBot
+
+  beforeEach(() => {
+    testBot = new BotPlayer('Test Bot Player')
+  })
+
+  describe('#selectPlayerToAskIndex', () => {
+    let game, player1, player2
+
+    beforeEach(() => {
+      player1 = new Player('Player 1')
+      player2 = new Player('Player 2')
+      player1._cards = [new Card('A', 'H')]
+      player2._cards = [new Card('2', 'H')]
+      testBot._cards = [new Card('3', 'H')]
+      game = new Game([player1, player2, testBot])
+    })
+
+    it('selects the first valid player', () => {
+      expect(testBot.selectPlayerToAskIndex(game)).toEqual(0)
+    })
+
+    it('never selects itself', () => {
+      game._players[0] = testBot
+      game._players[2] = player1
+      expect(testBot.selectPlayerToAskIndex(game)).toEqual(1)
+    })
+
+    it("won't select a player that doesn't have any cards", () => {
+      player1._cards = []
+      expect(testBot.selectPlayerToAskIndex(game)).toEqual(1)
+    })
+  })
+
+  describe('#selectRankToAskFor', () => {
+    beforeEach(() => testBot._cards = [new Card('7', 'D'), new Card('4', 'H')])
+    it('returns the rank of a randomly selected card in the hand of the bot player', () => {
+      expect(testBot.cards().map(card => card.rank())).toContain(testBot.selectRankToAskFor())
+    })
+  })
+})
