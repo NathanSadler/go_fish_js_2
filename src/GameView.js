7c1b5ebd54f411f3736bcff05ab5a2695ed93162
Nathan Sadler <nathan@sadlerville.com>
2021-09-15 11:08:28 -0400

    add basic gameView

--- src/GameView.js
+++ src/GameView.js
@@ -12,9 +12,32 @@ class GameView extends React.Component {
     super(props)
   }
 
-  render() {
-    const listItems = this.props.game.players().map(player => <li key={player.name()}>{player.name()}</li>)
+  generatePlayerInfo() {
+    const listItems = this.props.game.players().map(player => <li id={player.name()} key={player.name()}>
+      {player.name()}, {player.cardCount()} card(s), {player.score()} book(s)</li>)
     return(<ul>{listItems}</ul>)
+  }
+
+  generateTakeTurnForm() {
+    return(
+      <form>
+        <select name="player_name" id="player_name">
+          {this.props.game.players().slice(1).map(player => <option key={player.name()} value={this.props.game.getIndexOfPlayer(player)}>{player.name()}</option>)}
+        </select>
+
+        <select name='card' id='card'>
+          {this.props.game.players()[0].cards().map(card => <option key={card.generateId()} value={card.generateId()}>{card.describe()}</option>)}
+        </select>
+      </form>
+    )
+  }
+
+  render() {
+    return(
+    <div>
+      {this.generatePlayerInfo()}
+      {this.generateTakeTurnForm()}
+    </div>)
   }
 }
 
--- src/__tests__/GameView.test.js
+++ src/__tests__/GameView.test.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import { render, fireEvent } from '@testing-library/react'
 import GameView from './../GameView.js'
 import Game from './../Game.js';
+import Card from './../Card.js'
 import Player from  './../Player.js'
 
 describe('GameView', () => {
   let player_list, view, container, game, wrapper
 
   beforeEach(() => {
-    player_list = [new Player("John"), new Player("Bob")]
+    const player_list = [new Player("John"), new Player("Bob")]
+    player_list[0].setHand([new Card("7", "D"), new Card("9", "H")])
+    player_list[1].setHand([new Card("9", "D"), new Card("10", "H"), new Card("J", "H")])
+    player_list[0].setScore(2)
+    player_list[1].setScore(0)
 
     game = new Game(player_list)
 
@@ -22,6 +27,29 @@ describe('GameView', () => {
       // debugger
       expect(wrapper.container.innerHTML).toContain('John')
       expect(wrapper.container.innerHTML).toContain('Bob')
+    })
+
+    describe('displaying the cards of each player', () => {
+      it('displays the cards in the hand of the first player', () => {
+        expect(wrapper.container.innerHTML).toContain('7 of Diamonds')
+        expect(wrapper.container.innerHTML).toContain('9 of Hearts')
+      })
+
+      it('does not display the cards for players other than the first', () => {
+        expect(wrapper.container.innerHTML).not.toContain('9 of Diamonds')
+        expect(wrapper.container.innerHTML).not.toContain('10 of Hearts')
+        expect(wrapper.container.innerHTML).not.toContain('Jack of Hearts')
+      })
+
+      it('displays the number of cards each player has', () => {
+        expect(wrapper.container.innerHTML).toContain('2 card(s)')
+        expect(wrapper.container.innerHTML).toContain('3 card(s)')
+      })
+
+      it('displays the score of each player', () => {
+        expect(wrapper.container.innerHTML).toContain('2 book(s)')
+        expect(wrapper.container.innerHTML).toContain('0 book(s)')
+      })
     })
   })
 })
